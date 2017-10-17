4a6d2140719bc68d2f4bd401cc1f82d13992137b
Matthew Young <matthewdyoung@gmail.com>
2017-10-17 16:59:42 +0200

    Pass!

--- src/EFCore.Cypher/Metadata/Conventions/Internal/GraphImmediateConventionScope.cs
+++ src/EFCore.Cypher/Metadata/Conventions/Internal/GraphImmediateConventionScope.cs
@@ -12,7 +12,23 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal
             MakeReadonly();
         }
 
+        public override InternalEntityBuilder OnEntityAdded(InternalEntityBuilder builder) {
+            if (builder.Metadata.Builder is null) {
+                return null;
+            }
+
+            foreach (var convention in _graphConventionSet.EntityAddedConventions) {
+                builder = convention.Apply(builder);
+                if (builder?.Metadata.Builder is null) {
+                    return null;
+                }
+            }
+
+            return builder;
+        }
+
         public override bool OnPropertyFieldChanged(InternalNodePropertyBuilder builder, FieldInfo oldFieldInfo) {
+            // TODO:
             if (builder.Metadata.Builder == null || 
                 builder.Metadata.DeclaringType.Builder == null) {
                 return false;        
